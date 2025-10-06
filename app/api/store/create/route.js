4a58ea9961c2bbfe0e3fdaabc5e29e05c79a6daf
GVBharadwaj18 <imgvb3189@gmmail.com>
2025-10-06 19:50:50 +0530

    minute bug fixed

--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -1,5 +1,5 @@
 import imagekit from "@/configs/imagekit";
-import {getAuth} from "@clerk/nextjs/server ";
+import {getAuth} from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
